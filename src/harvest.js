f485a4a9d95f944c051d4f6a46e3af6145886f8c
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-05-15 14:05:00 +0300

    Dependency & Maintenance update (#64)
    * Bump the production-dependencies group with 1 update (#53)
    Bumps the production-dependencies group with 1 update: [@natlibfi/melinda-record-import-commons](https://github.com/natlibfi/melinda-record-import-commons-js).
    
    * Bump the development-dependencies group with 6 updates (#61)
    
    | Package | From | To |
    | --- | --- | --- |
    | [@babel/cli](https://github.com/babel/babel/tree/HEAD/packages/babel-cli) | `7.23.4` | `7.24.5` |
    | [@babel/core](https://github.com/babel/babel/tree/HEAD/packages/babel-core) | `7.23.7` | `7.24.5` |
    | [@babel/node](https://github.com/babel/babel/tree/HEAD/packages/babel-node) | `7.22.19` | `7.23.9` |
    | [@babel/preset-env](https://github.com/babel/babel/tree/HEAD/packages/babel-preset-env) | `7.23.8` | `7.24.5` |
    | [eslint](https://github.com/eslint/eslint) | `8.56.0` | `8.57.0` |
    | [nodemon](https://github.com/remy/nodemon) | `3.0.3` | `3.1.0` |
    
    * Bump @natlibfi/eslint-config-melinda-backend (#63)
    
    * Bump @natlibfi/melinda-commons in the production-dependencies group (#62)
    
    * Bump mikaelvesavuori/license-compliance-action from 1.0.2 to 1.0.3 (#54)
    
    * Update deps
    * Node-tests: add v22, remove v19
    * Update license to MIT
    
    * 2.0.5-alpha.1

--- src/harvest.js
+++ src/harvest.js
@@ -1,33 +1,3 @@
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* Helmet record harvester for the Melinda record batch import system
-*
-* Copyright (C) 2018 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of melinda-record-import-harvester-publication-archives
-*
-* melinda-record-import-harvester-publication-archives program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* melinda-record-import-harvester-publication-archives is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
-
-/* eslint-disable no-unused-vars, no-console */
-
 import fs from 'fs';
 import {URL} from 'url';
 import moment from 'moment';
